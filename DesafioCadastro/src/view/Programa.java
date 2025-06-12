1ce67eb8ca7b4968a602c4e0cd8c779716093728
Caio Romão <caiodev14@gmail.com>
2025-06-12 10:57:53 -0300

    feature: Adição do menu e exceções com entradas inválidas

--- DesafioCadastro/src/view/Programa.java
+++ DesafioCadastro/src/view/Programa.java
@@ -1,12 +1,35 @@
 package view;
 
-import model.entidades.File;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Programa {
     public static void main(String[] args) {
-        File.criarFormulario();
-        for(String pergunta : File.getLinhas()){
-            System.out.println(pergunta);
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("1.Cadastrar um novo pet");
+            System.out.println("2.Alterar os dados do pet cadastrado");
+            System.out.println("3.Deletar um pet cadastrado");
+            System.out.println("4.Listar todos os pets cadastrados");
+            System.out.println("5.Listar pets por algum critério (idade, nome, raça)");
+            System.out.println("5.Sair");
+
+            int op;
+
+            try {
+                op = sc.nextInt();
+                if (op <= 0) {
+                    throw new IllegalArgumentException("Digite uma ação Válida.");
+                }
+
+            } catch (InputMismatchException e) {
+                System.out.println("Erro: Digite apenas números.");
+                sc.nextLine();
+            } catch (IllegalArgumentException e) {
+                System.out.println("Erro: " + e.getMessage());
+            }
         }
     }
 }
